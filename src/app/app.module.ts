dd1fb10d39295bc265509d4361028d857e2d1b6e
Voke <vokebmax@loud-20.04>
2021-09-07 19:20:15 -0400

    Second Lab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BottomNavBarComponent } from './footer/bottom-nav-bar.components';
+import { SecondBottomNavBarComponent } from './footer/second-bottom-nav-bar.component';
+import { SecondNavBarComponent } from './header/second-nav-bar.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    SecondNavBarComponent,
+    BottomNavBarComponent,
+    SecondBottomNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/footer/second-bottom-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-second-bottom-nav-bar',
+    templateUrl: 'second-bottom-nav-bar.component.html'
+})
+
+export class SecondBottomNavBarComponent {
+
+}
