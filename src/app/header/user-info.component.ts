d044fa8b1b71678d8c888f744851c801e7a5da83
vokeBMax <vrb0002@mix.wvu.edu>
2021-10-19 18:33:52 -0400

    Assignment 7

--- src/app/header/user-info.component.ts
+++ src/app/header/user-info.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, Injectable, OnInit } from "@angular/core";
 import { UserInfo } from "./user-info.model";
+import { UserInfoService } from "./user-info.service";
 
-@Injectable()
 @Component ({
     selector: "offerup-user-info",
     templateUrl: "user-info.component.html"
@@ -10,20 +10,16 @@ import { UserInfo } from "./user-info.model";
 
 export class UserInfoComponent implements OnInit{
     myInfo: UserInfo | undefined;
-    constructor (private http: HttpClient) {
+    constructor (private infoService: UserInfoService) {
     }
     ngOnInit(): void {
-        console.log("Sending a get request to the server");
-        this.getUserInfo();
         console.log("Registering showUserInfo as a subscriber");
         this.showUserInfo();
     }
 
-    getUserInfo() {
-        return this.http.get<UserInfo>('https://offerup-app-b0852-default-rtdb.firebaseio.com/my-info.json');
-    }
+    
     showUserInfo() {
-        this.getUserInfo().subscribe((data:UserInfo) => {
+        this.infoService.getUserInfo().subscribe((data:UserInfo) => {
             console.log(data);
             this.myInfo = data;
         })
